5fa5db76a5abbe8b3c91b6b67016fa0865dd3679
Brad Gignac <brad.gignac@rackspace.com>
2013-03-18 09:45:48 -0400

    [Feature][81] Add event helper functions.

--- spec/unit/canon_spec.js
+++ spec/unit/canon_spec.js
@@ -1,4 +1,4 @@
-define(['canon', 'canon/dom'], function (canon, dom) {
+define(['canon', 'canon/dom', 'canon/events'], function (canon, dom, events) {
   describe('canon', function () {
     it('defines the canon namespace', function () {
       canon.should.exist;
@@ -6,6 +6,10 @@ define(['canon', 'canon/dom'], function (canon, dom) {
 
     it('adds the dom helper', function () {
       canon.dom.should.equal(dom);
+    });
+
+    it('adds the event helper', function () {
+      canon.events.should.equal(events);
     });
   });
 });
