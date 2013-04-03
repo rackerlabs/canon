5dc5c25fb0f99c0bb0c2bc0ed8194848c54a97c7
Brad Gignac <brad.gignac@rackspace.com>
2013-04-03 13:54:59 -0400

    [Maintenance][97] Extract common component functionality into base class.

---
+++ spec/unit/canon/core/component_spec.js
@@ -0,0 +1,32 @@
+define(['ender-amd', 'canon/core/component'], function ($, Component) {
+  describe('Component', function () {
+
+    var element, component;
+
+    beforeEach(function () {
+      fixture.innerHTML = '<div class="component"></div>';
+
+      element = $('.component');
+      component = new Component();
+      component.attach(element);
+    });
+
+    afterEach(function () {
+      component.dispose();
+    });
+
+    describe('#attach', function () {
+      it('fails when already attached', function () {
+        (function () { component.attach(element); }).should.throws('Component is already attached.');
+      });
+    });
+
+    describe('#dispose', function () {
+      it('discards DOM reference', function () {
+        component.dispose();
+
+        expect(component.getElement()).to.equal(null);
+      });
+    });
+  });
+});
