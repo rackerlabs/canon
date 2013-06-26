bc0695226873538c639abc16e9e6b799fd076693
Brad Gignac <brad.gignac@rackspace.com>
2013-06-26 15:16:50 -0400

    [Release][1.0.0-rc.1] Bump version.

--- lib/canon.rb
+++ lib/canon.rb
@@ -6,10 +6,10 @@ require 'sprockets'
 require 'sprockets/sass'
 
 module Canon
-  MAJOR = 0
-  MINOR = 5
+  MAJOR = 1
+  MINOR = 0
   PATCH = 0
-  PRE = nil
+  PRE = 'rc.1'
 
   class << self
     def version
