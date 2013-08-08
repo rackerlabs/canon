1c744571448c14f5751bef1cb98fe52ec3c30ab1
Brad Gignac <bgignac@bradgignac.com>
2013-08-08 15:59:23 -0400

    [Bug][232] Add acceptance tests for single menu changes.

--- spec/functional/menu_spec.rb
+++ spec/functional/menu_spec.rb
@@ -1,6 +1,5 @@
 feature 'Menu' do
   let(:dropdown) { find('.rs-dropdown') }
-  let(:toggle) { find('.rs-dropdown-toggle') }
   let(:menu) { find('.rs-dropdown-menu') }
 
   background do
@@ -21,7 +20,7 @@ feature 'Menu' do
     end
 
     it 'is visible after clicking toggle' do
-      toggle.click
+      click_link "I'm the toggle."
 
       menu.should be_visible
     end
@@ -30,7 +29,7 @@ feature 'Menu' do
   context 'Visible' do
     background do
       click_button 'Attach'
-      toggle.click
+      click_link "I'm the toggle."
     end
 
     it 'is hidden after clicking body' do
@@ -40,7 +39,7 @@ feature 'Menu' do
     end
 
     it 'is hidden after clicking toggle' do
-      toggle.click
+      click_link "I'm the toggle."
 
       menu.should_not be_visible
     end
@@ -51,108 +50,18 @@ feature 'Menu' do
       menu.should_not be_visible
     end
 
+    it 'is hidden after opening other menu' do
+      click_link "I'm another toggle."
+
+      all_menus = all('.rs-dropdown-menu')
+      all_menus.first.should_not be_visible
+      all_menus.last.should be_visible
+    end
+
     it 'remains open after clicking category' do
       menu.find('.rs-dropdown-category').click
 
       menu.should be_visible
     end
   end
-
-  # context 'Loading' do
-  #   background do
-  #     click_button 'Attach'
-  #   end
-
-  #   it 'does not change when header is clicked' do
-  #     header.click
-
-  #     assert_section_is_loading
-  #   end
-
-  #   it 'collapses when "Collapse" is clicked' do
-  #     click_button 'Collapse'
-
-  #     assert_section_is_collapsed
-  #   end
-
-  #   it 'expands when "Expand" is clicked' do
-  #     click_button 'Expand'
-
-  #     assert_section_is_expanded
-  #   end
-
-  #   it 'expands when "Toggle" is clicked' do
-  #     click_button 'Toggle'
-
-  #     assert_section_is_expanded
-  #   end
-  # end
-
-  # context 'Collapsed' do
-  #   background do
-  #     click_button 'Attach'
-  #     click_button 'Collapse'
-  #   end
-
-  #   it 'expands when header is clicked' do
-  #     header.click
-
-  #     assert_section_is_expanded
-  #   end
-
-  #   it 'expands when "Expand" is clicked' do
-  #     click_button 'Expand'
-
-  #     assert_section_is_expanded
-  #   end
-
-  #   it 'expands when "Toggle" is clicked' do
-  #     click_button 'Toggle'
-
-  #     assert_section_is_expanded
-  #   end
-  # end
-
-  # context 'Expanded' do
-  #   background do
-  #     click_button 'Attach'
-  #     click_button 'Expand'
-  #   end
-
-  #   it 'collapses when header clicked' do
-  #     header.click
-
-  #     assert_section_is_collapsed
-  #   end
-
-  #   it 'collapses when "Collapse" is clicked' do
-  #     click_button 'Collapse'
-
-  #     assert_section_is_collapsed
-  #   end
-
-  #   it 'collapses when "Toggle" is clicked' do
-  #     click_button 'Toggle'
-
-  #     assert_section_is_collapsed
-  #   end
-  # end
-
-  # def assert_section_is_loading
-  #   page.should have_css('.collapsible-section.loading')
-  #   page.should_not have_css('.collapsible-section.collapsed')
-  #   content.should_not be_visible
-  # end
-
-  # def assert_section_is_expanded
-  #   page.should_not have_css('.collapsible-section.loading')
-  #   page.should_not have_css('.collapsible-section.collapsed')
-  #   content.should be_visible
-  # end
-
-  # def assert_section_is_collapsed
-  #   page.should_not have_css('.collapsible-section.loading')
-  #   page.should have_css('.collapsible-section.collapsed')
-  #   content.should_not be_visible
-  # end
 end
