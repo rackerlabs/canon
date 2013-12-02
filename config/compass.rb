5fda843276a836fe7ae0c0f17b2dc11edf25c1b0
Ed Rooth <ed.rooth@rackspace.com>
2013-12-02 14:01:01 -0800

    [FEATURE] Use compass "font-face" mixin.

--- config/compass.rb
+++ config/compass.rb
@@ -2,6 +2,7 @@ images_dir = 'lib/images'
 javascripts_dir = 'lib/javascripts'
 sass_dir = 'lib/stylesheets'
 css_dir = 'dist'
+http_fonts_path = './'
 generated_images_dir = 'dist'
 http_generated_images_path = './'
 
