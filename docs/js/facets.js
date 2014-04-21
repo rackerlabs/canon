922ba1f4c5b5559f23ea22a6e333f12b61700a03
Andrew Raiford <andrew.raiford@rackspace.com>
2014-04-21 16:47:13 -0500

    [Enhancement][Issue-17] Changes facets.js to put control over showing/hiding of facet items in the JS rather than the css

--- docs/js/facets.js
+++ docs/js/facets.js
@@ -1,10 +1,12 @@
+var facetLimit = 5;
+
 var setClearAllState = function() {
   var isListFaceted = $('.rs-facet-section .selected').length;
 
   if(isListFaceted) {
-    $('.rs-facets-header .rs-clear-facets-link').removeClass('rs-hidden');
+    $('.rs-facet-header .rs-facet-clear-link').removeClass('rs-hidden');
   } else {
-    $('.rs-facets-header .rs-clear-facets-link').addClass('rs-hidden');
+    $('.rs-facet-header .rs-facet-clear-link').addClass('rs-hidden');
   }
 }
 
@@ -12,13 +14,51 @@ var setClearSectionState = function(section) {
   var isSectionFaceted = section.find('.selected').length;
 
   if(isSectionFaceted) {
-    section.find('.rs-facets-section-header .rs-clear-facets-link').removeClass('rs-hidden');
+    section.find('.rs-facet-section-header .rs-facet-clear-link').removeClass('rs-hidden');
   } else {
-    section.find('.rs-facets-section-header .rs-clear-facets-link').addClass('rs-hidden');
+    section.find('.rs-facet-section-header .rs-facet-clear-link').addClass('rs-hidden');
   }
 }
 
-$(document).ready(function() {
+var toggleSection = function(section) {
+  var toggleHTML = '';
+  if(section.hasClass('collapsed')) {
+    toggleHTML = '<i class="rs-facet-toggle-arrow"></i>less';
+    section.removeClass('collapsed').addClass('expanded').find('.rs-facet-item.rs-hidden').removeClass('rs-hidden');
+  } else {
+    toggleHTML = '<i class="rs-facet-toggle-arrow"></i>more';
+    section.removeClass('expanded').addClass('collapsed');
+    var i = 0;
+    section.find('.rs-facet-item').each(function() {
+      if(i>=facetLimit) {
+        $(this).addClass('rs-hidden');
+      }
+      i++;
+    });
+  }
+  section.find('.rs-facet-section-toggle').html(toggleHTML);
+}
+
+var initFacets = function() {
+
+  $('.rs-facet-section').each(function() {
+    var section = $(this);
+    var items = section.find('.rs-facet-item');
+    if(items.length >= facetLimit+1) {
+      var i=0;
+      items.each(function() {
+        if(i>=facetLimit) {
+          $(this).addClass('rs-hidden');
+        }
+        i++;
+      });
+      section.addClass('collapsed');
+
+      if(!section.find('.rs-facet-section-toggle').length) {
+        section.find('.rs-facet-list').append('<li class="rs-facet-section-toggle"><i class="rs-facet-toggle-arrow"></i>more</li>');
+      }
+    }
+  });
 
   $('.rs-facet-section .rs-facet-item').click(function() {
     $(this).toggleClass('selected');
@@ -26,33 +66,22 @@ $(document).ready(function() {
     setClearSectionState($(this).closest('.rs-facet-section'));
   });
 
-  $('.rs-facets-header .rs-clear-facets-link').click(function() {
+  $('.rs-facet-header .rs-facet-clear-link').click(function() {
     $(this).addClass('rs-hidden');
     $('.rs-facet-section .rs-facet-item.selected').removeClass('selected');
-    $('.rs-facet-section .rs-clear-facets-link').addClass('rs-hidden');
+    $('.rs-facet-section .rs-facet-clear-link').addClass('rs-hidden');
   });
 
   $('.rs-facet-section-toggle').click(function() {
-    var thisToggle = $(this);
-    var thisSection = $(this).closest('.rs-facet-section');
-    var isCollapsed = thisSection.hasClass('collapsed');
-
-    var more = '<i class="rs-arrow"></i>more';
-    var less = '<i class="rs-arrow"></i>less';
-
-    if(isCollapsed) {
-      thisSection.addClass('expanded').removeClass('collapsed');
-      thisToggle.html(less);
-    } else {
-      thisSection.addClass('collapsed').removeClass('expanded');
-      thisToggle.html(more);
-    }
-
+    toggleSection($(this).closest('.rs-facet-section'));
   });
 
-  $('.rs-facets-section-header .rs-clear-facets-link').click(function() {
+  $('.rs-facet-section-header .rs-facet-clear-link').click(function() {
     $(this).addClass('rs-hidden').closest('.rs-facet-section').find('.selected').removeClass('selected');
     setClearAllState();
   });
+}
 
+$(document).ready(function() {
+  initFacets();
 });
